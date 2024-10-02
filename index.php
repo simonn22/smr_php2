9c15beb34bc3ea37b0347f3c519f1e29a8b133c7
Simon Jimenez Crespo <jimenezcresposimon@gmail.com>
2024-10-02 15:02:08 +0200

    login con mensaje de error

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ error_reporting(E_ALL);
 <head>
 <style>
 	b{
-		color:red;
+		color:darkgreen;
 	}
 	h1{
 		
@@ -30,6 +30,10 @@ error_reporting(E_ALL);
 		text-align: center;
 		font-size: 16px;
 	}
+	.error{
+	color:white;
+	background-color: darkred;
+	}
 
 }
 </style>
@@ -42,24 +46,30 @@ error_reporting(E_ALL);
 
 if(!isset($_SESSION['id_user'])){
 
-	if (isset($_POST['passwd']) and $_POST['passwd']=='enchegado' and $_POST['user']=='smr'){
+	if (isset($_POST['passwd'])){
+		if( $_POST['passwd']=='enchegado' and $_POST['user']=='smr'){
 		$_SESSION['id_user']=1;
 	}
 	
-	
-	else{
-		echo '<form method="post" class="formulario">'
-		.'<input placeholder="Usuario" name="user" type "user">'
-		.'<input placeholder="Contraseña" name="passwd" type "password">'
-		.'<div><button>Envía usuario y contraseña</button></div>'
-		.'</form>';
+	else
+		echo '<div class="error">Usuario o contraseña incorrectos</div>';
+}
 }	
+
+if(!isset($_SESSION ['id_user'])){
+	echo '<form method="post" class="formulario">'
+	.'<input placeholder="Usuario" name="user" type "user">'
+	.'<input placeholder="Contraseña" name="passwd" type ="password">'
+	.'<div><button>Envía usuario y contraseña</button></div>'
+	.'</form>';
+
+
+
 }
 
 if(isset($_SESSION['id_user'])){
 	echo '<div><img src = "CHEMA.png" width=400>';
-	echo '<a href="?logout=1">Cerrar sesión</a>';
+	echo '<div><a href="?logout=1">Cerrar sesión</a></div>';
 
 }
-?>
-</body>
+
