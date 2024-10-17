83ff93195986f4aeab810320e745b6be5ddf02ef
Simon <jimenezcresposimon@gmail.com>
2024-10-17 10:23:24 +0200

    todo chupiguay

--- index.php
+++ index.php
@@ -12,80 +12,22 @@ error_reporting(E_ALL);
 <head>
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Afacad+Flux:wght@100..1000&display=swap" rel="stylesheet"><style>
-	b{
-		color:darkgreen;
-	}
-	h1{
-		font-size: 55px;
-		color: black;	
-		text-shadow: 0 0 5px grey;
-	}
-	body{
-	font-family: "Afacad Flux";
-	color: aliceblue;
-	text-align: center;
-	background-color: black;
-	}
-	.container{
-		display:flex;
-		font-family: "Afacad Flux";
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		width: 100%;
-		height: 100vh;
-	}
-	.box {
-		padding: 50px;
-		padding-top: 20px;
-		background-color: white; 
-		border-radius: 50px;
-		border: 0px solid #555; 
-		box-shadow: 0px 0px 50px rgb(240, 240, 240); 
-		box-sizing: border-box;
-	}
-	.formulario input,button{
-		font-family: "Afacad Flux";
-		margin: 10px;
-		border-style: solid;
-		border-radius: 60px;
-		border-color: black;
-		width: auto;
-		padding: 15px 30px;
-		text-align: center;
-		font-size: 30px;
-		background-color: black;
-		color: white;
-	}
-	.div_button{
-		width: auto;
-		align-items: center;
-	}
-	.error{
-	color:white;
-	background-color: darkred;
-	padding: 10px;
-	border-radius: 25px;
-	margin: 10px;
-	font-size: 20px;
-	}
-	.image{
-		border-radius: 20px;
-		box-shadow: 0px 0 30px grey;
-	}
+<link href="https://fonts.googleapis.com/css2?family=Afacad+Flux:wght@100..1000&display=swap" rel="stylesheet">
 
 
-</style>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="styles.css">
+
 </head>
 <body>
 
 <div class = "container">
 	<div class = "box">
-<h1>INICIO DE SESIÓN</h1>
+
 	
 <?php
-
 if(!isset($_SESSION['id_user'])){
 
 	if (isset($_POST['passwd'])){
@@ -104,19 +46,12 @@ if(!isset($_SESSION['id_user'])){
 		}
 		else echo '<div class="error">Usuario incorrecto</div>';
 	
-
-		/*
-		if( $_POST['passwd']=='enchegado' and $_POST['user']=='smr'){
-		$_SESSION['id_user']=1;
-	}
-	
-	else
-		echo '<div class="error">Usuario o contraseña incorrectos</div>';
-	*/
 }
 }	
 
 if(!isset($_SESSION ['id_user'])){
+	
+	echo '<h1>INICIO DE SESIÓN</h1>';
 	
 	echo '<form method="post" class="formulario">'
 	.'<input placeholder="Usuario" name="user" type "user">'
@@ -139,5 +74,7 @@ if(isset($_SESSION['id_user'])){
 	</div>
 	<div class = "container">
 	<?php
-	echo '<div><a href="register.php"><button>Registrarse</button></a></div>';
+	if(!isset($_SESSION['id_user'])){
+		echo '<div><a href="register.php"><button>Registrarse</button></a></div>';
+	}
 	
--- register.php
+++ register.php
@@ -12,78 +12,25 @@ error_reporting(E_ALL);
 <head>
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Afacad+Flux:wght@100..1000&display=swap" rel="stylesheet"><style>
-	b{
-		color:darkgreen;
-	}
-	h1{
-		font-size: 55px;
-		color: black;	
-		text-shadow: 0 0 5px grey;
-	}
-	body{
-	font-family: "Afacad Flux";
-	color: aliceblue;
-	text-align: center;
-	background-color: black;
-	}
-	.container{
-		display:flex;
-		font-family: "Afacad Flux";
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		width: 100%;
-		height: 100vh;
-	}
-	.box {
-		padding: 50px;
-		padding-top: 20px;
-		background-color: white; 
-		border-radius: 50px;
-		border: 0px solid #555; 
-		box-shadow: 0px 0px 50px rgb(240, 240, 240); 
-		box-sizing: border-box;
-	}
-	.formulario input,button{
-		font-family: "Afacad Flux";
-		margin: 10px;
-		border-style: solid;
-		border-radius: 60px;
-		border-color: black;
-		width: auto;
-		padding: 15px 30px;
-		text-align: center;
-		font-size: 30px;
-		background-color: black;
-		color: white;
-	}
-	.div_button{
-		width: auto;
-		align-items: center;
-	}
-	.error{
-	color:white;
-	background-color: darkred;
-	padding: 10px;
-	border-radius: 25px;
-	margin: 10px;
-	font-size: 20px;
-	}
-	.image{
-		border-radius: 20px;
-		box-shadow: 0px 0 30px grey;
-	}
+<link href="https://fonts.googleapis.com/css2?family=Afacad+Flux:wght@100..1000&display=swap" rel="stylesheet">
 
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="styles.css">
 
-</style>
+	<script>
+function redirectToIndex() {
+    setTimeout(function() {
+        window.location.href = 'index.php';
+    }, 3000); // Redirige después de unos segundos
+}
+</script>
 </head>
 <body>
 
 <div class = "container">
 	<div class = "box">
-<h1>INICIO DE SESIÓN</h1>
-	
 <?php
 
 if(!isset($_SESSION['id_user'])){
@@ -93,30 +40,19 @@ if(!isset($_SESSION['id_user'])){
 		$r=$conn->query("
 			INSERT INTO usuarios (usuario, passwd) VALUES
             ('".$_POST['user']."', '".$_POST['passwd']."');
-		")->fetch_assoc();
-		//echo '----->'.$r['id_usuario'].'<-----';
-		if(isset($r['id_usuario'])){
-			if(isset($_POST['passwd']) and $_POST['passwd']==$r['passwd']){
-				$_SESSION['id_user']=$r['id_usuario'];
-			}
-			else echo '<div class="error">Contraseña incorrecta</div>';
-		}
-		else echo '<div class="error">Usuario incorrecto</div>';
-	
-
-		/*
-		if( $_POST['passwd']=='enchegado' and $_POST['user']=='smr'){
-		$_SESSION['id_user']=1;
-	}
-	
-	else
-		echo '<div class="error">Usuario o contraseña incorrectos</div>';
-	*/
+		");
+		if ($r) {
+            echo '<p class = registro_completo>Registro completo, se redirigirá al inicio de sesión.</p>';
+            echo '<script>redirectToIndex();</script>'; // Redirige después de mostrar el mensaje
+        } 
+		else {
+            echo '<p>Hubo un error en el registro. Inténtalo de nuevo.</p>';
+        }
 }
 }	
 
-if(!isset($_SESSION ['id_user'])){
-	
+if(!isset($_SESSION ['id_user']) and !isset($r)){
+	echo '<h1>REGISTRARSE</h1>';
 	echo '<form method="post" class="formulario">'
 	.'<input placeholder="Usuario" name="user" type "user">'
 	.'<div><input placeholder="Contraseña" name="passwd" type ="password">'
